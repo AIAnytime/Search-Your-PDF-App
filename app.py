f9e80acb4b873c95c959e7a34dc84e0016262a68
Somesh <97947690+Somesh140@users.noreply.github.com>
2024-04-07 00:01:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,48 @@
-import streamlit as st 
+import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
 import torch
 import base64
 import textwrap
-from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.vectorstores import Chroma 
-from langchain.llms import HuggingFacePipeline
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
-from constants import CHROMA_SETTINGS
 
-#model and tokenizer loading
+# model and tokenizer loading
 checkpoint = "LaMini-T5-738M"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-base_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
+base_model = AutoModelForSeq2SeqLM.from_pretrained(
+    checkpoint, 
+    device_map='cpu', 
+    torch_dtype=torch.float32)
+
 
 @st.cache_resource
 def llm_pipeline():
     pipe = pipeline(
         'text2text-generation',
-        model = base_model,
-        tokenizer = tokenizer,
-        max_length = 256,
+        model=base_model,
+        tokenizer=tokenizer,
+        max_length=256,
         do_sample=True,
-        temperature = 0.3,
-        top_p = 0.95
+        temperature=0.3,
+        top_p=0.95
     )
     local_llm = HuggingFacePipeline(pipeline=pipe)
     return local_llm
+
 
 @st.cache_resource
 def qa_llm():
     llm = llm_pipeline()
     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    db = Chroma(persist_directory="db", embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    db = Chroma(persist_directory="db", embedding_function=embeddings)
     retriever = db.as_retriever()
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    qa = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
     return qa
+
 
 def process_answer(instruction):
     response = ''
@@ -46,12 +52,13 @@ def process_answer(instruction):
     answer = generated_text['result']
     # metadata = generated_text['metadata']
     # for text in generated_text:
-        
+
     #     print(answer)
 
     # wrapped_text = textwrap.fill(response, 100)
     # return wrapped_text
-    return answer,generated_text
+    return answer, generated_text
+
 
 def main():
     st.title("Search Your PDF 🐦📄")
@@ -73,4 +80,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
